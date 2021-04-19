1f09d9ffffdefc2816a9823dd5a0572071193536
BritNS24 <bri24@umbc.edu>
2021-04-18 20:14:42 -0400

    Drive functions

--- Functions/google-drive/drive-test.py
+++ Functions/google-drive/drive-test.py
@@ -1,57 +1,54 @@
 from pydrive.auth import GoogleAuth
+#Authorizes google drive access
 
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
-gauth.SaveCredentialsFile("mycred.txt")
+def driveAuthentic():
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+    gauth.SaveCredentialsFile("mycred.txt") #saves credentials to a file
+    drive = GogleDrive(gauth)
+    return drive 
 
-drive = GogleDrive(gauth)
+def create(user_title, user_content):
+    userfile= drive.CreateFile({'title': user_title})
+    userfile.SetContentString(user_content)
+    userfile.Upload()
+    return userfile
+
+def list_of_files()
+    file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+    for file1 in file_list:
+    print('title: %s, id: %s' % (file1['title'], file1['id']))
+
+
+def download_files(download_title)
+    file_list = drive.ListFile({'q': "title = " +str(user_title)+ "and trashed = false"}).GetList()
+    print(file_list[0]['title'])
+    file_id = file_list[0]['id']
+
+    file = drive.CreateFile({'id':file_id})
+    file.GetContentFile(download_title)
+
+def trash_files(files)
+    files.Trash()
+
+def update_title(files, user_title,new_title)
+    files[user_title] = new_title
+    files.upload
+    return files
+
+def update_content(files, )
+    files.SetContentString(user_content)
+    files.Upload
+    return files
+
+
+
+
+
+
 
 #variables
 user_title
 user_content
-new_title
-
-
-#to create doc and upload to user drive
-
-file= drive.CreateFile({'title': 'Just Testing.txt'})
-file.SetContentString("user content goes here")
-file.Upload()
-
-
-#To search for files, the following are possible commands
-#trashed = true
-#title contains ... and title contains ..
-#title = ...
-
-file_list = drive.ListFile({'q': "title contains 'user_title' and trashed = false" }).GetList()
-print(file_list[0]['title']
-file_id = file_list[0]['id'])
-
-#downloading files as a local copy
-file= drive.CreateFile({'id': file_id})
-file.GetContentFile('title')
-
-
-#sending local file to drive
-new_title = drive.CreateFile()
-new_title.SetContentFile('user_content)')
-
-
-
-
-#create image file
-
-    file2 = drive.CreateFile()
-    file2.SetContentFile(user_content)
-    file2.Upload()
-    print('Created file %s with mimeType %s' % (file2['title'],
-    file2['mimeType']))
-
-
-
- 
-
-
-
+download_title
 
