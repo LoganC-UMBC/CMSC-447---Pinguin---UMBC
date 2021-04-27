36903bda1a0f718df7ef88352aaf1f68a884b81c
s99-git <70349633+s99-git@users.noreply.github.com>
2021-04-26 21:27:13 -0400

    fixed imports for db copy in application/database folder

--- Application/Database/PinguinDB.py
+++ Application/Database/PinguinDB.py
@@ -1,7 +1,7 @@
 import pymongo, datetime, time, hashlib
 
 from bson.objectid import ObjectId
-from Database.utils.class_user import User
+from Application.Database.utils.class_user import User
 from random import seed, randint
 
 
@@ -14,6 +14,8 @@ class PinguinDB:
             "mongodb+srv://PinguinAdmin:D47b5PIrtJxIhnuV@pinguin-cluster.vvukk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
         # self.client = pymongo.MongoClient("mongodb+srv://guest:kYmIc3X8vSHcNTfy@pinguin-cluster.vvukk.mongodb.net/Pinguin?retryWrites=true&w=majority")
 
+        # Definitions for the different databases
+        # Databases that end in Pinguin do not have a different collection for each group
         self.groups = self.client.Groups.Pinguin
         self.users = self.client.Users.Pinguin
         self.invites = self.client.Invites
@@ -22,30 +24,37 @@ class PinguinDB:
         self.documents = self.client.Documents
         self.tasks = self.client.Tasks
 
-        self.db = self.client.Pinguin
-        self.usersOld = self.db.Users
-        self.groupsOld = self.db.Groups
-
-    # changes default user credentials
+    # Changes stored user credentials
     def change_user(self, user_id, name, pword, _id, groups):
         self.user.user_id = user_id
-        self.user.name = name
+        self.user.user_name = name
         self.user.pword = pword
         self.user._id = _id
         self.user.groups = groups
+        # Sets the current group to be the first group the user has joined if they are in one
+        # Can be changed later to save group on exit and use that as default group
+        if (len(self.user.groups) >= 1):
+            self.user.currentGroup = self.user.groups[0]
         return 1
 
-    # will check
+    # Logs in the user
     def login(self, user_id, pword):
-        m = hashlib.sha256(pword.encode())
-        if (self.users.count_documents({'user_id': user_id} and {'pass': m.hexdigest()})):
-            user_doc = self.users.find_one({'user_id': user_id} and {'pass': m.hexdigest()})
-            self.change_user(user_doc['user_id'], user_doc['name'], user_doc['pass'], user_doc['_id'],
+        # Encrypts password so that it is not stored as plaintext
+        encPass = hashlib.sha256(pword.encode())
+        # "encPass.hexdigest" is the alphanumeric representation of the encrypted password
+        # Checks if a user exists with the entered username and password
+
+        if (self.users.count_documents({'user_id': user_id})):
+
+            user_doc = self.users.find_one({'user_id': user_id})
+            if user_doc['pass'] != encPass.hexdigest():
+                print("Incorrect password")
+                return 0
+            # Updates variables for logged in user's information, then confirms login
+            self.change_user(user_doc['user_id'], user_doc['user_name'], user_doc['pass'], user_doc['_id'],
                              user_doc['groups'])
             print("login successful")
 
-            if (len(self.user.groups) >= 1):
-                self.user.defaultGroup = self.user.groups[0]
             return 1
         else:
             print('login unsucessful')
@@ -55,29 +64,41 @@ class PinguinDB:
         userList = []
         userList.append(self.user._id)
         userList.append(self.user.user_id)
-        userList.append(self.user.name)
-
+        userList.append(self.user.user_name)
+        print(userList[0])
+        print(userList[1])
+        print(userList[2])
         return userList
 
-    # def change_group(self, group_name):
-    # if(self.users.find_one({"groups":group_name})):
-    # self.user
+    # Changes the current group to another group the user is in
+    def change_group(self, group_id):
 
+        if (group_id in self.user.groups):
+            self.user.currentGroup = group
+            print(self.user.currentGroup)
+            return 1
+        else:
+            print("User is not a member of given group")
+            return 0
+
+    # Create a new account with the entered details
     def create_account(self, user_id, pword, name):
+        # Rejects the request if an account with that email already exists
         if (self.users.find_one({"user_id": user_id})):
             print('User already exists')
             return 0
         else:
             self.user.user_id = user_id
-            m = hashlib.sha256(pword.encode())
-            self.user.pword = m.hexdigest()
-            self.user.name = name
+            encPass = hashlib.sha256(pword.encode())
+            self.user.pword = encPass.hexdigest()
+            self.user.user_name = name
 
             user_account = {"user_id": self.user.user_id,
-                            "name": self.user.name,
+                            "user_name": self.user.user_name,
                             "pass": self.user.pword,
                             "role": "user",
-                            "groups": []}
+                            "groups": [],
+                            "invites": []}
 
             self.users.insert_one(user_account)
 
@@ -85,21 +106,32 @@ class PinguinDB:
 
             return 1
 
+    # Create a group
     def create_group(self, name, description):
-        if (self.groups.find_one({"name": name})):
+        if (self.groups.find_one({"group_name": name})):
             print('Group of that name already exists')
             return 0
         else:
-            # invites_split = invites.split(",")
-            # "invites": invites_split,
-            group_post = {"name": name,
-                          "owner": self.user.name,
+
+            group_post = {"group_name": name,
+                          "owner": self.user._id,
                           "description": description,
-                          "members": [self.user.user_id]}
+                          "members": [self.user._id],
+                          "invites": []}
 
             self.groups.insert_one(group_post)
 
-            self.forums[name]
+            groupFind = self.groups.find_one({"group_name": name}).get("_id")
+            self.user.groups.append(groupFind)
+            self.users.update_one({"_id": self.user._id}, {"$push": {"groups": groupFind}})
+
+    # Checks if the user is the owner of the group
+    def check_ownership(self, group_id, user_id):
+        groupCheck = self.groups.find_one({"_id": group_id}).get("owner")
+        if user == groupCheck:
+            return 1
+        else:
+            return 0
 
     def testConnections(self):
         print(self.db.list_collection_names())
@@ -107,12 +139,10 @@ class PinguinDB:
     def get_groups(self):
         groupList = []
         for x in self.groups.find():
-            if x.get("name") in self.user.groups:
+            if x.get("_id") in self.user.groups:
                 groupList.append(x)
-                # print(x)
+                print(x)
         return groupList
-
-    # Refresh handlers
 
     def refresh_tasks(self):
         return
@@ -131,84 +161,133 @@ class PinguinDB:
     def refresh_all(self):
         return
 
-    def calendar_add(self):
-        return
+    def send_invite(self, reciever_id):
 
-    def group_create(self, group_name, group_users):
-        if (group_users != None):
-            group = {'group_name': group_name,
-                     'owner': self.user._id,
-                     'users': [user for user in group_users]
-                     }
-            group['users'].append(self.user.name)
+        if (not self.users.find_one({"_id": reciever_id})):
+            print("User not found")
+            return 0
 
-        else:
-            group = {'group_name': group_name,
-                     'owner': self.user.name,
-                     'users': 'None'
-                     }
-        self.db.Groups.insert_one(group)
-        return 1
-
-    def send_invites(self, invites):
-        post = {'sender': self.user.name,
-                'receivers': [user for user in invites]
+        post = {'sender_name': self.user.user_name,
+                'sender': self.user._id,
+                'group_id': self.user.currentGroup,
+                'receiver': reciever_id
                 }
+
+        self.users.update_one({"_id": reciever_id}, {"$push": {"invites": post}})
+        self.groups.update_one({"_id": self.user.currentGroup}, {"$push": {"invites": post}})
+
         return 1
 
     # Forums Window
+    def invite_accept(self, group_id):
 
-    def receive_invites(self):
+        # remove the invite
 
-        return
+        inviteList = self.users.find_one({"_id": self.user._id}).get("invites")
+        for x in inviteList:
+            if x['group_id'] == group_id:
+                inviteFind = x
 
-    def send_post(self, group, message):
-        post = {'author': self.user.name,
-                'group': group,
+        self.groups.update_one({"_id": group_id}, {"$unset": {"invites": inviteFind}})
+        self.users.update_one({"_id": self.user._id}, {"$unset": {"invites": inviteFind}})
+
+        # add them to the group
+        self.user.groups.append(group_id)
+        self.users.update_one({"_id": self.user._id}, {"$push": {"groups": group_id}})
+        self.groups.update_one({"_id": group_id}, {"$push": {"members": self.user._id}})
+        return 1
+
+    def invite_deny(self, group_id):
+        inviteList = self.users.find_one({"_id": self.user._id}).get("invites")
+        for x in inviteList:
+            if x['group_id'] == group_id:
+                inviteFind = x
+
+        self.groups.update_one({"_id": group_id}, {"$unset": {"invites": inviteFind}})
+        self.users.update_one({"_id": self.user._id}, {"$unset": {"invites": inviteFind}})
+
+        return 1
+
+    def send_post(self, message):
+        post = {'author': self.user._id,
+                'group': self.user.currentGroup,
                 'message': message,
                 'date': datetime.datetime.utcnow()
                 }
 
-        self.forums[group].insert_one(post)
+        self.forums[self.user.currentGroup].insert_one(post)
         return 1
 
     def retrieve_all_posts(self, group):
-        posts = []
-        for x in self.forums[group].find():
-            posts.append(x)
+        postList = []
+        for x in self.forums[self.user.currentGroup].find():
+            print(x)
+            postList.append(x)
+        return postList
 
-        return posts
+    def calendar_add(self, description, date):
 
-    def calendar_add(self, group, description, date):
-
-        calendarPost = {'author': self.user.name,
-                        'group': group,
+        calendarPost = {'author': self.user._id,
+                        'group': self.user.currentGroup,
                         'day': date.day,
                         'month': date.month,
                         'year': date.year,
                         'description': description
                         }
-        self.calendars[group].insert_one(calendarPost)
+        self.calendars[self.user.currentGroup].insert_one(calendarPost)
 
-    def retrieve_calendar_posts(self, group, date, time):
+        return 1
 
+    def retrieve_calendar_posts(self, date, time):
+        calendarList = []
         if time == "month":
-            for x in self.calendars[group].find({'month': date.month}):
-                print("flag1")
+            for x in self.calendars[self.user.currentGroup].find({'month': date.month}):
                 print(x)
+                calendarList.append[x]
         elif time == "year":
-            for x in self.calendars[group].find({'year': date.year}):
+            for x in self.calendars[self.user.currentGroup].find({'year': date.year}):
                 print(x)
+                calendarList.append[x]
         elif time == "day":
-            for x in self.calendars[group].find({'day': date.day}):
+            for x in self.calendars[self.user.currentGroup].find({'day': date.day}):
                 print(x)
-        # not done, needs to diferentiate between month and year
+                calendarList.append[x]
 
-    def receive_posts(self):
-        return
+        return calendarList
 
-        # Error Management
+    def document_add(self, title, link):
 
+        post = {
+            'author': self.user.user_name,
+            'group': self.user.currentGroup,
+            'title': title,
+            'doc': link
+        }
+
+        self.documents[self.user.currentGroup].insert_one(post)
+        return 1
+
+    def retrieve_docs(self):
+        docList = []
+        for x in self.documents[self.user.currentGroup].find():
+            docList.append(x)
+        return docList
+
+    def user_lookup(self, user_id):
+        if (self.users.find_one({'_id': user_id})):
+            userFind = self.users.find_one({'_id': user_id})
+            return userFind
+        else:
+            return 0
+
+    def group_lookup(self, group_id):
+        if (self.groups.find_one({'_id': group_id})):
+            groupFind = self.groups.find_one({'_id': group_id})
+            return groupFind
+        else:
+            return 0
+
+    # Error Management
     def errorHandler(self, error):
 
         return
