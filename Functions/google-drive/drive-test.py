4b69dd19d8215373ce2ff0c25ef2770707fd09cf
BritNS24 <bri24@umbc.edu>
2021-04-27 18:45:44 -0400

    Changed drive

--- Functions/google-drive/drive-test.py
+++ Functions/google-drive/drive-test.py
@@ -1,54 +1,25 @@
 from pydrive.auth import GoogleAuth
 #Authorizes google drive access
 
-def driveAuthentic():
+
+
+def authenticate ():
     gauth = GoogleAuth()
     gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
-    gauth.SaveCredentialsFile("mycred.txt") #saves credentials to a file
-    drive = GogleDrive(gauth)
-    return drive 
+    #gauth.SaveCredentialsFile("mycred.txt") #saves credentials to a file
+    drive = GoogleDrive(gauth)
 
-def create(user_title, user_content):
-    userfile= drive.CreateFile({'title': user_title})
-    userfile.SetContentString(user_content)
-    userfile.Upload()
-    return userfile
+#create file
+def create(user_title):
 
-def list_of_files()
-    file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
-    for file1 in file_list:
-    print('title: %s, id: %s' % (file1['title'], file1['id']))
+    user_file = drive.CreateFile({'title': user_title})
+    user_file.Upload()
 
 
-def download_files(download_title)
-    file_list = drive.ListFile({'q': "title = " +str(user_title)+ "and trashed = false"}).GetList()
-    print(file_list[0]['title'])
-    file_id = file_list[0]['id']
+def trash_files(trash_file):
+    #creates an instance of the file 
+    file2 = drive.CreateFile({'title': trash_file})
+    #puts file into trash folder
+    file2.Trash()
 
-    file = drive.CreateFile({'id':file_id})
-    file.GetContentFile(download_title)
-
-def trash_files(files)
-    files.Trash()
-
-def update_title(files, user_title,new_title)
-    files[user_title] = new_title
-    files.upload
-    return files
-
-def update_content(files, )
-    files.SetContentString(user_content)
-    files.Upload
-    return files
-
-
-
-
-
-
-
-#variables
-user_title
-user_content
-download_title
 
