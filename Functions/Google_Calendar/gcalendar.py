1f95db6ab89de157954ff61162451f5c6f4cdf9c
Xillow <Matthewbee1@gmail.com>
2021-05-02 14:09:57 -0400

    Fixed AddtoCalendarList and adjusted getMonthEvents

--- Functions/Google_Calendar/gcalendar.py
+++ Functions/Google_Calendar/gcalendar.py
@@ -4,12 +4,9 @@ from pydrive.auth import GoogleAuth
 
 """
 must edit auth.py in PyDrive for GoogleAuth to work with Google Calendar's APIs
-
 within the directory that calls GoogleAuth a settings.yaml file must be included
 with the scope for google calendar. the scope level used to allow every function
 to opperate is "https://www.googleapis.com/auth/calendar"
-
-
 """
 
 
@@ -19,7 +16,6 @@ class GoogleCalendar(object):
 
     def __init__(self, auth=None):
         """gives access to the Google Account given
-
         :param auth: authorized GoogleAuth instance.
         :type auth: pydrive.auth.GoogleAuth.
         """
@@ -29,13 +25,10 @@ class GoogleCalendar(object):
 
         """Creates a calendar.
            First account that creates it owns the calendar.
-
         :param calendarTitle: parameter that determines the title of the calendar
         :type param: str
-
         :param timeZone: parameter that determines the time zone offset
         :type param:str
-
         """
 
 
@@ -53,8 +46,7 @@ class GoogleCalendar(object):
         """deletes the calendar.
            Every account that had the calendar will no longer
            have access to the calendar
-
-        :param calendarId: parameter that google calendar service uses
+        :param calId: parameter that google calendar service uses
                            to determine which calendar to opperate within
         :type param: str
         """
@@ -65,20 +57,24 @@ class GoogleCalendar(object):
 
     def AddToCalendarList(self, calId=None):
         """Adds the calendar to the associated accounts calendar list
-
-        :param calendarId: parameter that google calendar service uses
+        :param calId: parameter that google calendar service uses
                            to determine which calendar to opperate within
         :type param: str
         """
 
+        groupCalendar = {
 
-        self.auth.service2.calendarList().insert(calendarId = calId).execute()
+            'id' : calId
 
-    def DeleteFromCalendarList(self, calID=None):
+        }
+
+
+        self.auth.service2.calendarList().insert(body = groupCalendar).execute()
+
+    def DeleteFromCalendarList(self, calId=None):
 
         """deletes the calendar from the associated accounts calendar list
-
-        :param calendarId: parameter that google calendar service uses
+        :param calId: parameter that google calendar service uses
                            to determine which calendar to opperate within
         :type param: str
         """
@@ -87,26 +83,19 @@ class GoogleCalendar(object):
 
     def CreateEvent(self, calId=None, eventTitle=None, eventDescription=None, startTime=None, endTime=None, timeZone=None):
         """creates the event to be inserted into the associated calendar
-
-        :param calendarId: parameter that google calendar service uses
+        :param calId: parameter that google calendar service uses
                            to determine which calendar to opperate within
         :type param: str
-
         :param eventTitle: parameter that determines the event name
         :type param:str
-
         :param eventTitle: parameter that determines the event description
         :type param:str
-
         :param startTime: parameter that determines the event start time
         :type param:str
-
         :param endTime: parameter that determines the event end time
         :type param:str
-
         :param timeZone: parameter that determines the time zone offset
         :type param:str
-
         """
 
         newEvent = {
@@ -124,33 +113,29 @@ class GoogleCalendar(object):
 
         self.auth.service2.events().insert(calendarId = calId, body=newEvent).execute()
 
-    def DeleteEvent(self, calID=None, currEventId=None):
+    def DeleteEvent(self, calId=None, currEventId=None):
         """Deletes an event in the associated calendar
-
-        :param calendarId: parameter that google calendar service uses
+        :param calId: parameter that google calendar service uses
                            to determine which calendar to opperate within
         :type param: str
-
-        :param eventId: parameter that google calendar service uses
+        :param currEventId: parameter that google calendar service uses
                         to determine which event to operate within
         :type param:str
         """
-        self.auth.service2.events().delete(calendarId = calId, eventId = currEventID).execute()
+        self.auth.service2.events().delete(calendarId = calId, eventId = currEventId).execute()
 
         #returns list of events
-    def getMonthEvents(self, monthBegin, monthEnd):
+    def getMonthEvents(self, calId = None, monthBegin = None, monthEnd = None):
         """
         monthBegin and monthEnd has to be in this format for GoogleCalendar to pull informaiton
         '2021-01-01'
-
         the only things that would need to change are year and month day should always be the first of the current month and first of the next month.
-
         """
         dateStart = monthBegin + 'T00:00:00' + 'Z' # 'Z' indicates UTC time
         dateEnd = monthEnd + 'T00:00:00' + 'Z' # 'Z' indicates UTC time
 
 
-        months_events = eventsself.auth.service2.events().list(calendarId='primary', timeMin=dateStart, timeMax=dateEnd, singleEvents=True, timeZone='America/New_York', orderBy='startTime').execute()
+        months_events = self.auth.service2.events().list(calendarId=calId, timeMin=dateStart, timeMax=dateEnd, singleEvents=True, timeZone='America/New_York', orderBy='startTime').execute()
 
         events = months_events.get('items', [])
 
@@ -158,19 +143,14 @@ class GoogleCalendar(object):
 
         """
         example call
-
             #for event in events:
             #    start = event['start'].get('dateTime', event['start'].get('date'))
             #    print(start,'\n', event['summary'],'\n',event['description'],'\n')
-
         event is an indexed dictionary entry
         link is to a overview of what the dictionary contains
-
         https://developers.google.com/calendar/v3/reference/events
-
         what's printed to console based on a test calendar entry
         2021-04-17T20:30:00-04:00
         Test 1
         Test 1
-
         """
